24c9b2b9881d3dcc0a71761d540a90c1830352fb
Ilya Shulenin <ilya@shulenin.com>
2020-06-13 08:15:12 +0300

    Добавил получение токена

--- main.php
+++ main.php
@@ -1,2 +1,21 @@
 <?php
-echo "Hello, world!";
+
+$credentials = [
+  'login' => 'rudemoru',
+  'password' => 'rudemo123456',
+];
+$url = 'https://online.omnicomm.ru/auth/login?jwt=1';
+
+$curl = curl_init($url);
+
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($credentials, '', '&'));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_HEADER, false);
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+var_dump(json_decode($result)->{'jwt'});
