5ee12567e19787e7ee306a4134d3c3742220a076
Ilya Shulenin <ilya@shulenin.com>
2020-06-13 17:05:29 +0300

    Добавил преобразование дерева в плоский массив

--- main.php
+++ main.php
@@ -4,30 +4,44 @@ $credentials = [
   'login' => 'rudemoru',
   'password' => 'rudemo123456',
 ];
-$authorization = 'https://online.omnicomm.ru/auth/login?jwt=1';
+$authorizationUrl = 'https://online.omnicomm.ru/auth/login?jwt=1';
 
 $curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL, $authorization);
+curl_setopt($curl, CURLOPT_URL, $authorizationUrl);
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($credentials, '', '&'));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_HEADER, false);
 
 $header[0] = 'Authorization: JWT '.json_decode(curl_exec($curl))->{'jwt'};
 
 curl_close($curl);
 
-$treeVehicle = 'https://online.omnicomm.ru/ls/api/v2/tree/vehicle';
+$treeVehicleUrl = 'https://online.omnicomm.ru/ls/api/v2/tree/vehicle';
 
 $curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL, $treeVehicle);
+curl_setopt($curl, CURLOPT_URL, $treeVehicleUrl);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$response = curl_exec($curl);
+$treeVehicles = json_decode(curl_exec($curl));
 
 curl_close($curl);
 
-var_dump($response);
+function flatVehicle($vehicles)
+{
+  $result = [];
+  $childrens = $vehicles->{'children'};
+  foreach($childrens as $children) {
+    flatVehicle($children);
+  }
+
+  $result = array_merge($result, $vehicles->{'objects'});
+  
+  return $result;
+}
+
+$vehicles = flatVehicle($treeVehicles);
+
+var_dump($vehicles);
